118aaee6e1ef8fe6cc47bfb5a432d846dbda6fd9
mluto <m@luto.at>
2013-04-07 14:58:47 +0200

    Backup doctexts

--- tasks/backup-docdata.js
+++ tasks/backup-docdata.js
@@ -1,3 +1,7 @@
+var async = require('async')
+  , fs    = require('fs')
+  , path  = require('path')
+  , showpadapi = require('../show-pad-api.js')
 
 exports.name = "docdata";
 exports.requiredConfig =
@@ -7,7 +11,71 @@ exports.requiredConfig =
   , "showpad:apikey"
   ];
 
-exports.execute = function (config, dest, logger, cb)
+var config, dest, logger;
+
+exports.execute = function (_config, _dest, _logger, cb)
 {
-  cb();
+  config = _config;
+  dest = _dest;
+  logger = _logger;
+
+  var url = "http://" + config.showpad.host + ":" + config.showpad.port;
+  showpadapi.init(url, config.showpad.apikey);
+  backupDocs(cb);
+}
+
+
+function backupDocs(cb)
+{
+  async.waterfall(
+    [
+      // get the data
+      function (cb)
+      {
+        showpadapi.get("doctexts", cb);
+      },
+      // save to disk
+      function (data, cb)
+      {
+        async.forEach(data,
+          function (doc, cb)
+          {
+            if(doc.error)
+            {
+              logger.error("Could not backup doc '%s': %s", doc.name, doc.error)
+            }
+            else
+            {
+              var file = path.join(dest, "bak-" + doc.name + ".json");
+              fs.writeFile(file, JSON.stringify(data),
+                function (err)
+                {
+                  if(err)
+                    cb(err);
+                  else
+                    cb(null, data.length);
+                }
+              );
+            }
+          },
+          function (err)
+          {
+            if(err)
+              cb(err);
+            else
+              cb(null, data.length);
+          }
+        );
+      }
+    ],
+    function (err, count)
+    {
+      if(err)
+        logger.error("There was an error while backing up the document-texts: %s", err);
+      else
+        logger.info("Backed up %s document-texts", count);
+
+      cb(err);
+    }
+  )
 }
